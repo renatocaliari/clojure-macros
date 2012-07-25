adb6d103bc8e27c4a9f95e998149092e0508f51a
Renato <renatoac82@gmail.com>
2012-07-25 00:56:30 -0300

    Update defchain.clj

--- defchain.clj
+++ defchain.clj
@@ -1,6 +1,9 @@
 (defmacro defchain [name functions]
-  "create a chain of calls among functions inside 'body'.
-   it will stop in the first function that it isn't able to handle the value.
+  "create a chain of calls among functions inside 'functions' vector.
+   it will stop at the first function that isn't able to handle the value.
    pattern: chain of responsability."
   `(let [chain-call# (reduce wrap ~functions)]
      (def ~(symbol name) chain-call#)))
+
+;; Example of use:
+;; (defchain name-my-var [function1 function2 function3]
